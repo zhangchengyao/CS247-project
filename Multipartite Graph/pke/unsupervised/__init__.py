58e181aa78879bd64d568ff749d9d0592ed32aca
chengyaoZhang <tc2002618@126.com>
2019-06-12 22:33:50 -0700

    add multipartite

---
+++ Multipartite Graph/pke/unsupervised/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Python Keyphrase Extraction toolkit: unsupervised models
+
+from __future__ import absolute_import
+
+from pke.unsupervised.graph_based.topicrank import TopicRank
+from pke.unsupervised.graph_based.multipartiterank import MultipartiteRank
